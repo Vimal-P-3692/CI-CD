2ec22b2ca76e25e1d00eb5ae7320e0f9b9656c05
Vimal-P-3692 <vimalp3692@gmail.com>
2026-03-28 20:56:49 +0530

    first commit

---
+++ src/test/java/com/example/demo/HelloControllerTest.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class HelloControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void testHelloEndpoint() {
+        String response = restTemplate.getForObject("/", String.class);
+        assertEquals("Hello World ...", response);
+    }
+}
